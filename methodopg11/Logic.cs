6bc84863a4ceb9e049b82a0572c57dfe3c4c4467
Emillarsen194 <47022984+Emillarsen194@users.noreply.github.com>
2019-02-17 20:14:07 +0100

    Add project files.

---
+++ methodopg11/Logic.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodopg11
+{
+    class Logic
+    {
+        //11.1 return the sum of 2 numbers
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        //11.2 divide to numbers 
+
+        public static int Divided(int a, int b)
+        {
+            return a / b;
+        }
+
+        // 11.3
+
+        public static double Modulus(double a, double b)
+        {
+            double howmany = a / b;
+            double remainder = a % b;
+
+            return howmany + remainder;
+        }
+
+        // 11.4 
+
+        public static List<int> BiggestElement(List<int> A)
+        {
+            Console.WriteLine(A.Max()); 
+
+            return A;
+        }
+
+        public static bool DoesItContain(List<string> A) //11.5 checks if a certain element is in our list 
+        {
+            bool contains = true;
+            if (A.Contains("x"))
+            {
+               contains = true;
+                 
+            }
+
+            else if (!A.Contains("x"))
+            {
+                contains = false;
+             
+            }
+
+            return contains;
+        }
+
+        // 11.6
+        public static double FindAverge(List<int> A, double howmanynumbers, double sum)
+        {
+            
+            double getit = howmanynumbers / sum ;
+           
+
+            return getit; 
+        }
+
+    }
+}
---
+++ methodopg11/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodopg11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = 16;
+            int numberTwo = 5;
+
+            // 11.1 return the sum 
+            Console.WriteLine(Logic.Sum(numberOne, numberTwo));
+            Console.WriteLine();
+
+            // 11.2 return two numbers divided by each other
+
+            Console.WriteLine(Logic.Divided(numberOne, numberTwo));
+            Console.WriteLine();
+
+            // 11.3 
+            Console.WriteLine(Logic.Modulus(numberOne, numberTwo));
+            Console.WriteLine();
+
+            //11.4
+
+            List<int> liste = new List<int> { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine(liste.Max());
+
+            Console.WriteLine(Logic.BiggestElement(liste));
+
+
+           Console.WriteLine(Logic.BiggestElement(liste));
+
+
+
+            //11.5 
+
+            List<string> listing = new List<string> { "a", "b", "c", "x"};
+            
+
+            Logic.DoesItContain(listing);
+
+            if (Logic.DoesItContain(listing) == true)
+            {
+                Console.WriteLine("true");
+            }
+
+            else if (Logic.DoesItContain(listing) == false)
+            {
+                Console.WriteLine("false");
+            }
+
+            //11.6 
+           
+            List<int> listOfNumbers = new List<int> { 10, 10, 10, 10 };
+            double howmanynumbers = listOfNumbers.Count;
+            double sum = listOfNumbers.Sum();
+            
+            Console.WriteLine(Logic.FindAverge(listOfNumbers, howmanynumbers, sum));
+
+            
+        }
+    }
+}
